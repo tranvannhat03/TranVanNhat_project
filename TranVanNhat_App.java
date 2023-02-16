62162fa124419e7ee27b15c41ff44fb5641cfd4d
Tran Van.Nhat <104410119+tranvannhat03@users.noreply.github.com>
2023-02-17 00:01:56 +0700

    Update TranVanNhat_App.java

--- TranVanNhat_App.java
+++ TranVanNhat_App.java
@@ -1,9 +1,6 @@
  public class  TranVanNhat_App {
          public static void main(String[] args) {
-              Systeam.out.println("Tran Van Nhat hoc Java.");
+              System.out.println("Tran Van Nhat hoc Java.");
          }
    
-   {
-        
-   
-   
+ }
